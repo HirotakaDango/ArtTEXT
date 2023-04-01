7efa1f5187cbbcb28e03f5b99c8d03ed4536a56a
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-04-01 15:57:09 +0700

    Add files via upload

--- session.php
+++ session.php
@@ -1,26 +1,53 @@
 <?php
 session_start();
 $db = new PDO('sqlite:database.db');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->exec("CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)");
 $db->exec("CREATE TABLE IF NOT EXISTS posts ( id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, synopsis TEXT NOT NULL, content TEXT NOT NULL, user_id INTEGER NOT NULL, cover BLOB NOT NULL, tags TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id))");
+
 if (isset($_POST['login'])) {
-  $username = $_POST['username'];
+  $username = trim($_POST['username']);
   $password = $_POST['password'];
+
+  if (empty($username) || empty($password)) {
+    echo 'Please enter username and password';
+    exit;
+  }
+
   $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
   $user = $db->query($query)->fetch();
+
   if ($user) {
     $_SESSION['user_id'] = $user['id'];
     header('Location: index.php');
+    exit;
   } else {
     echo 'Invalid username or password';
+    exit;
   }
 } elseif (isset($_POST['register'])) {
-  $username = $_POST['username'];
+  $username = trim($_POST['username']);
   $password = $_POST['password'];
+
+  if (empty($username) || empty($password)) {
+    echo 'Please enter username and password';
+    exit;
+  }
+
+  $query = "SELECT * FROM users WHERE username='$username'";
+  $user = $db->query($query)->fetch();
+
+  if ($user) {
+    echo 'Username already taken';
+    exit;
+  }
+
   $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
   $db->exec($query);
+
   $_SESSION['user_id'] = $db->lastInsertId();
   header('Location: index.php');
+  exit;
 }
 ?>
 
