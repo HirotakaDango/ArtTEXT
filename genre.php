40908bdd29267671655d7705f74d95ed0ba9d69c
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-04-16 14:46:12 +0700

    Add files via upload

--- genre.php
+++ genre.php
@@ -1,30 +1,41 @@
 <?php
-session_start();
-$db = new PDO('sqlite:database.db');
-if (!isset($_SESSION['user_id'])) {
-  header('Location: session.php');
-}
+// connect to the database
+$pdo = new PDO('sqlite:database.db');
 
-$db = new PDO('sqlite:database.db');
-$db->exec("CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)");
-$db->exec("CREATE TABLE IF NOT EXISTS posts ( id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, synopsis TEXT NOT NULL, content TEXT NOT NULL, user_id INTEGER NOT NULL, tags TEXT NOT NULL, date DATETIME, FOREIGN KEY (user_id) REFERENCES users(id))");
-
+// set the number of posts per page
 $posts_per_page = 100;
-$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-$start_index = ($page - 1) * $posts_per_page;
 
-$query = "SELECT * FROM posts ORDER BY id DESC LIMIT $start_index, $posts_per_page";
-$posts = $db->query($query)->fetchAll();
+// get the tag from the URL parameter
+$tag = isset($_GET['tag']) ? $_GET['tag'] : '';
 
-$count_query = "SELECT COUNT(*) FROM posts";
-$total_posts = $db->query($count_query)->fetchColumn();
+// count the total number of posts with the given tag
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM posts WHERE tags LIKE :tag');
+$stmt->bindValue(':tag', '%'.$tag.'%');
+$stmt->execute();
+$total_posts = $stmt->fetchColumn();
+
+// calculate the total number of pages
 $total_pages = ceil($total_posts / $posts_per_page);
+
+// get the current page from the URL parameter
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+// calculate the offset
+$offset = ($page - 1) * $posts_per_page;
+
+// query the database for the posts on the current page
+$stmt = $pdo->prepare('SELECT * FROM posts WHERE tags LIKE :tag ORDER BY id DESC LIMIT :limit OFFSET :offset');
+$stmt->bindValue(':tag', '%'.$tag.'%');
+$stmt->bindValue(':limit', $posts_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
   <head>
-    <title>ArtTEXT</title>
+    <title>Posts by Genre: <?php echo $tag ?></title>
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
@@ -32,7 +43,8 @@ $total_pages = ceil($total_posts / $posts_per_page);
   </head>
   <body>
     <?php include('header.php'); ?>
-    <div class="mt-4">
+    <p class="ms-3 mt-3 fw-bold">Genre: <?php echo $tag ?></p> 
+    <div class="mt-2">
       <div class="container-fluid text-center">
         <div class="contents">
           <?php foreach ($posts as $post): ?>
@@ -46,14 +58,14 @@ $total_pages = ceil($total_posts / $posts_per_page);
         </div>
         <div class="pagination mt-4 justify-content-center">
           <?php if ($page > 1): ?>
-            <a class="btn btn-sm fw-bold btn-primary me-1" href="?page=<?php echo $page - 1 ?>">Prev</a>
+            <a class="btn btn-sm fw-bold btn-primary me-1" href="?tag=<?php echo $tag ?>&page=<?php echo $page - 1 ?>">Prev</a>
           <?php endif ?>
           <?php if ($page < $total_pages): ?>
-            <a class="btn btn-sm fw-bold btn-primary ms-1" href="?page=<?php echo $page + 1 ?>">Next</a>
-          <?php endif ?>
+            <a class="btn btn-sm fw-bold btn-primary ms-1" href="?tag=<?php echo $tag ?>&page=<?php echo $page + 1 ?>">Next</a>
+          <?php endif ?> 
         </div>
       </div>
-    </div>
+    </div> 
     <style>
       .contents {
         display: grid;
--- view.php
+++ view.php
@@ -36,7 +36,17 @@ $user_posts = $db->query($user_posts_query)->fetchAll();
       <h1 class="text-center fw-semibold"><?php echo isset($post['title']) ? $post['title'] : '' ?></h1>
       <p class="mt-5">Author: <?php echo isset($post['username']) ? $post['username'] : '' ?></p>
       <p class="mt-2">Published: <?php echo isset($post['date']) ? $post['date'] : '' ?></p>
-      <p class="mt-2">Genre: <?php echo isset($post['tags']) ? $post['tags'] : '' ?></p>
+      <p class="mt-2">Genre:
+      <?php if (isset($tag)) {
+        echo '<a class="btn btn-secondary btn-sm rounded-pill fw-bold" href="genre.php">All</a> ';
+        }
+        if (isset($post['tags'])) {
+          $tags = explode(',', $post['tags']);
+          foreach ($tags as $tag) {
+            echo '<a class="btn btn-secondary btn-sm rounded-pill fw-bold" href="genre.php?tag=' . urlencode(trim($tag)) . '">' . trim($tag) . '</a> ';
+          }
+        } ?>
+      </p>
       <p class="mt-3">Synopsis</p> 
       <small class="container font-l"><?php echo isset($post['synopsis']) ? $post['synopsis'] : '' ?></small> 
       <hr>
