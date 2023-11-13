8056c230d818026dcdd5dc221e0c9699c271e10b
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-11-13 09:58:48 +0700

    Add files via upload

--- header.php
+++ header.php
@@ -4,8 +4,24 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-          <ul class="navbar-nav">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+          <div class="position-absolute start-50 translate-middle-x d-none d-md-block">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active' ?>" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'upload.php') echo 'active' ?>" href="upload.php">Upload</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'profile.php') echo 'active' ?>" href="profile.php">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'setting.php') echo 'active' ?>" href="setting.php">Setting</a>
+              </li>
+            </ul>
+          </div>
+          <ul class="navbar-nav d-md-none d-lg-none">
             <li class="nav-item">
               <a class="nav-link fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active' ?>" href="index.php">Home</a>
             </li>
@@ -19,9 +35,11 @@
               <a class="nav-link fw-bold <?php if(basename($_SERVER['PHP_SELF']) == 'setting.php') echo 'active' ?>" href="setting.php">Setting</a>
             </li>
           </ul>
+          <form class="d-flex ms-auto" action="genre.php" role="search">
+            <input class="form-control me-2" name="tag" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
         </div>
       </div>
     </nav>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
     <br><br>
