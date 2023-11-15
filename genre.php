f9b20c90065cda25d96277e97666fa4ce812d285
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-11-15 09:08:59 +0700

    Add files via upload

--- genre.php
+++ genre.php
@@ -49,22 +49,24 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php foreach ($posts as $post): ?>
           <div class="col">
             <a class="content text-decoration-none" href="view.php?id=<?php echo $post['id'] ?>">
-              <div class="card shadow-sm h-100 d-flex justify-content-center align-items-center text-center position-relative">
-                <i class="bi bi-book-half display-1 p-5 text-secondary border-bottom w-100"></i>
-                <h5 class="border-bottom w-100 p-3"><?php echo $post['title']; ?></h5>
+              <div class="card shadow-sm h-100 position-relative">
+                <div class="d-flex justify-content-center align-items-center text-center">
+                  <i class="bi bi-book-half display-1 p-5 text-secondary border-bottom w-100"></i>
+                </div>
+                <h5 class="border-bottom text-center w-100 p-3"><?php echo $post['title']; ?></h5>
                 <div class="card-body">
                   <?php
                     // Get the full description
-                    $fullDesc = $post['content'];
+                    $fullDesc = $post['synopsis'];
 
                     // Limit the description to 120 characters (words)
-                    $limitedDesc = substr($post['content'], 0, 120);
+                    $limitedDesc = substr($post['synopsis'], 0, 120);
 
                     // Find the position of the last word within the first 120 characters
                     $lastSpacePos = strrpos($limitedDesc, ' ');
 
                     // Check if the full description is longer than the limited description
-                    if (strlen($post['content']) > strlen($limitedDesc)) {
+                    if (strlen($post['synopsis']) > strlen($limitedDesc)) {
                       // If it is, add a "full view" link
                       $limitedDesc = substr($limitedDesc, 0, $lastSpacePos).'...';
                     }
